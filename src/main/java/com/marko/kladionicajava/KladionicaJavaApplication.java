676429ee1bd92c1d9c00f3d093319975634a071e
marko <marko.milosevic2202@gmail.com>
2023-07-11 19:14:22 +0200

    /initialization

---
+++ src/main/java/com/marko/kladionicajava/KladionicaJavaApplication.java
@@ -0,0 +1,13 @@
+package com.marko.kladionicajava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KladionicaJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KladionicaJavaApplication.class, args);
+	}
+
+}
