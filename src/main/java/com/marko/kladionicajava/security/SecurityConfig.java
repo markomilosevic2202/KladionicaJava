f0eb9b7ad78606118fc15c2ccf76e29b2abc31ee
marko <marko.milosevic2202@gmail.com>
2023-09-18 07:21:23 +0200

    /security

--- src/main/java/com/marko/kladionicajava/security/SecurityConfig.java
+++ src/main/java/com/marko/kladionicajava/security/SecurityConfig.java
@@ -1,25 +1,18 @@
 package com.marko.kladionicajava.security;
 
 
-import com.marko.kladionicajava.controller.HomeController;
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-
 import javax.sql.DataSource;
 
-//@Configuration
-//@EnableWebSecurity
+@Configuration
+@EnableWebSecurity
 
 
 public class SecurityConfig {
